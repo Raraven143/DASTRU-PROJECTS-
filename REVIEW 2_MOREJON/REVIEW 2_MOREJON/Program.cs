db02df8d344ee1cb71dcd7c614f8d0f3ef404da8
Papellero <ACT-STUDENT@LAB3-PC28>
2025-12-10 08:56:01 +0800

    ERGRSDG

---
+++ REVIEW 2_MOREJON/REVIEW 2_MOREJON/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REVIEW_2_MOREJON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        Circle circle1 = new Circle();
+            circle1.Color = "Black";
+            circle1.Radius = 10;
+
+            circle1.GetArea();
+            circle1.CalculateArea(10);
+
+         Rectangle rectangle1 = new Rectangle();
+            rectangle1.Color = "Pink";
+            rectangle1.Width = 6.7;
+            rectangle1.Height = 12.5;
+            rectangle1.GetArea();
+            rectangle1.CalculateArea(6.7 * 12.5);
+        }
+    }
+}
---
+++ REVIEW 2_MOREJON/REVIEW 2_MOREJON/Shape.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REVIEW_2_MOREJON
+{
+    abstract class Shape
+    {
+        //MOREJON, RAVEN T. | IT401A | REVIEW 2:  Object - Oriented Programming
+        public string Color { get; set; }
+
+
+        public virtual void GetArea()
+        {
+            Console.WriteLine($"The color of the shape is {Color}");
+        }
+
+        public abstract void CalculateArea(double Area);
+    }
+
+        class Circle : Shape
+        {
+            public double Radius { get; set; }
+
+            public override void GetArea()
+            {
+                base.GetArea();
+                Console.WriteLine($"The radius of the Circle: {Radius}");
+            }
+
+            public override void CalculateArea(double Area)
+            {
+                double circleArea = 3.14 * Radius;
+                Console.WriteLine($"The area of the Circle: {circleArea}");
+            }
+        }
+
+            class Rectangle : Shape
+            {
+                public double Width { get; set; }  
+                public double Height { get; set; }
+                
+                public override void GetArea()
+                {
+                    base.GetArea();
+                    Console.WriteLine($"The Width of the Rectangle: {Width}");
+                    Console.WriteLine($"The Height of the Rectangle: {Height}");
+                }
+
+                public override void CalculateArea(double Area)
+                {
+                   double rectangleArea = Width * Height;
+                    Console.WriteLine($"The Area of the Rectangle: {rectangleArea}");
+
+                }
+
+            }
+    }
+        
+
